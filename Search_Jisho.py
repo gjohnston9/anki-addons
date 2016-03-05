399041bc6663a6b4ab0d5f9fe4c3f6fdb19a2341
Greg Johnston <greg.johnston@gatech.edu>
2016-03-05 00:27:02 -0500

    added header

--- Search_Jisho.py
+++ Search_Jisho.py
@@ -1,3 +1,10 @@
+"""
+An add-on that provides extra functionality for studying Japanese using Anki.
+Press 8 to search Jisho.org for the text in the question field of the current card.
+Press 9 to search Jisho for sentences containing the question text.
+Press 0 to search Jisho for kanji details for the question text.
+"""
+
 SEARCH_URL = 'http://jisho.org/search/%s'
 SEARCH_SENTENCES_URL = 'http://jisho.org/search/%s%%20%%23sentences'
 SEARCH_KANJI_DETAILS_URL = 'http://jisho.org/search/%s%%20%%23kanji'
@@ -12,7 +19,12 @@ def keyHandler(self, evt, _old):
     if key == "8" or key == "9" or key == "0":
         q = aqt.mw.reviewer.card.q()
         encoded = q[q.rfind(">") + 1:].encode('utf8', 'ignore') # search term is the portion of card.q() following the last occurrence of ">"
-        search = (SEARCH_URL * (key == "8")) + (SEARCH_SENTENCES_URL * (key == "9")) + (SEARCH_KANJI_DETAILS_URL * (key == "0"))
+        if key == "8":
+        	search = SEARCH_URL
+        elif key == "9":
+        	search = SEARCH_SENTENCES_URL
+        else:
+        	search = SEARCH_KANJI_DETAILS_URL
         url = aqt.qt.QUrl.fromEncoded(search % (urllib.quote(encoded)))
         aqt.qt.QDesktopServices.openUrl(url)
     else:
